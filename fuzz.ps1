f92df70dfe6e7bc8583786a99221e4ff25ea92cd
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-27 19:24:35 +0200

    Add fuzz.ps1

---
+++ fuzz.ps1
@@ -0,0 +1,49 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$project,
+    [Parameter(Mandatory = $true)]
+    [string]$corpus,
+    [Parameter(Mandatory = $true)]
+    [string]$targetDll,
+    [string]$dict = $null,
+    [int]$timeout = 10
+)
+
+Set-StrictMode -Version Latest
+
+$libFuzzer = "libfuzzer-dotnet-windows.exe"
+$uri = "https://github.com/metalnem/libfuzzer-dotnet/releases/latest/download/$libFuzzer"
+
+Invoke-WebRequest -Uri $uri -OutFile $libFuzzer
+
+$outputDir = "bin"
+
+if (Test-Path $outputDir) {
+    Remove-Item -Recurse -Force $outputDir
+}
+
+dotnet tool install --global SharpFuzz.CommandLine
+dotnet publish $project -c release -o $outputDir --self-contained
+
+$projectName = (Get-Item $project).BaseName
+$project = Join-Path $outputDir $projectName
+$fuzzingTarget = Join-Path $outputDir $targetDll
+
+Write-Output "Instrumenting $fuzzingTarget"
+
+$env:SHARPFUZZ_INSTRUMENT_MIXED_MODE_ASSEMBLIES = 1
+sharpfuzz $fuzzingTarget
+
+if ($LastExitCode -ne 0) {
+    Write-Error "An error occurred while instrumenting $fuzzingTarget"
+    exit 1
+}
+
+Write-Output "$dict"
+
+if ($dict) {
+    & "./$libFuzzer" -timeout="$timeout" -dict="$dict" --target_path=$project $corpus
+}
+else {
+    & "./$libFuzzer" -timeout="$timeout" --target_path=$project $corpus
+}
--- src/JsonSerializerFuzzer/Program.cs
+++ src/JsonSerializerFuzzer/Program.cs
@@ -7,19 +7,14 @@ namespace JsonSerializerFuzzer;
 
 public class Item
 {
-    public byte A { get; set; } = 123;
-    public int B { get; set; } = 1024;
-    public double C { get; set; } = 3.1415;
-    public DateTime D { get; set; } = new DateTime(2010, 10, 15, 8, 45, 30, 875);
-    public string E { get; set; } = "2.71828";
-    public short[] F { get; set; } = new short[] { 1, 2, 3, 4 };
-    public Item G { get; set; } = new Item { A = 1 };
-
-    public Dictionary<string, int> H { get; set; } = new Dictionary<string, int>
-    {
-        ["10"] = 10,
-        ["20"] = 20
-    };
+    public byte A { get; set; }
+    public int B { get; set; }
+    public double C { get; set; } 
+    public DateTime D { get; set; }
+    public string E { get; set; }
+    public short[] F { get; set; }
+    public Item G { get; set; }
+    public Dictionary<string, int> H { get; set; }
 }
 
 public class Program
