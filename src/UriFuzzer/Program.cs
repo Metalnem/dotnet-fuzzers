aa4efccd7e6c26cfa1482147ae9ee958c38ab43f
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-28 15:09:00 +0200

    Add Uri fuzzer

---
+++ src/UriFuzzer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using SharpFuzz;
+
+namespace UriFuzzer;
+
+public class Program
+{
+    public static void Main()
+    {
+        Fuzzer.LibFuzzer.Run(span =>
+        {
+            try
+            {
+                var s = Encoding.UTF8.GetString(span);
+                Uri.TryCreate(s, UriKind.RelativeOrAbsolute, out _);
+            }
+            catch { }
+        });
+    }
+}
