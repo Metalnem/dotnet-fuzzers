203c4202b9c648b6f9d7cf9f750fb918794ab2e7
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-28 15:45:06 +0200

    Add certificate fuzzer

---
+++ src/X509Certificate2Fuzzer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using SharpFuzz;
+
+namespace X509Certificate2Fuzzer;
+
+public class Program
+{
+    public static void Main()
+    {
+        Fuzzer.LibFuzzer.Run(span =>
+        {
+            try
+            {
+                var s = Encoding.ASCII.GetString(span);
+                X509Certificate2.CreateFromPem(s);
+            }
+            catch { }
+        });
+    }
+}
