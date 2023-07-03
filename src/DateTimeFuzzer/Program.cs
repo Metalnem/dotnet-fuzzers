d2b844dbfb32cfc09e48cad3ba8e7a7f776e39e6
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-07-03 18:47:22 +0200

    Add DateTime fuzzer

---
+++ src/DateTimeFuzzer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using SharpFuzz;
+
+namespace DateTimeFuzzer;
+
+public class Program
+{
+    public static void Main()
+    {
+        Fuzzer.LibFuzzer.Run(span =>
+        {
+            var s1 = Encoding.UTF8.GetString(span);
+
+            if (DateTime.TryParse(s1, out var dt1))
+            {
+                var s2 = dt1.ToString("O");
+                var dt2 = DateTime.Parse(s2, null, DateTimeStyles.RoundtripKind);
+
+                if (dt1 != dt2)
+                {
+                    throw new Exception();
+                }
+            }
+        });
+    }
+}
